57a8b6307adcc80217bfda87afc7fe39fdca927a
cheukyin699 <cheukyin699@yahoo.com>
2015-08-30 15:49:35 -0700

    Added license

--- disassembler/Disassembler.cpp
+++ disassembler/Disassembler.cpp
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Chip8_Dissassembler.
+ *
+ * Chip8_Disassembler is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Chip8_Disassembler is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Chip8_Disassembler.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <cstdio>
 #include <cstring>
 #include <iostream>
